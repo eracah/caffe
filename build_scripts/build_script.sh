7ddf6b6e2616f2c02ee618d1c0a79e1868369ad6
eracah <ejracah@gmail.com>
2017-02-01 15:22:27 -0800

    modified build script to not copy .git files

--- build_scripts/build_script.sh
+++ build_scripts/build_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -l
 #EXPECTS TO BE RUN FROM OUTSIDE OF CAFFE REPO (AKA ../..)
-cp -rp caffe caffe_built
-cd caffe_built
+rm -rf caffe_built
+mkdir caffe_built
+cd caffe
+cp -r `ls -A  | grep -v ".git"` ../caffe_built
+cd ../caffe_built
 export PATH=/usr/common/software/python/2.7-anaconda/envs/caffe_env/bin/:$PATH
 make clean
 make all -j 12
 make pycaffe
-cd ..
+
