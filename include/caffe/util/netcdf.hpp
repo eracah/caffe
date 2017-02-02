7bbc5f1fe24a592f3e5bbce604411d44035468d9
Evan Racah <eracah@lbl.gov>
2017-02-02 11:46:29 -0800

    removed more commented out code

--- include/caffe/util/netcdf.hpp
+++ include/caffe/util/netcdf.hpp
@@ -27,18 +27,10 @@ namespace caffe {
 	template <typename Dtype>
 	void netcdf_load_nd_dataset_transposed(const int& file_id, const std::vector<string>& netcdf_variables_,const int& time_stride, const int& min_dim, const int& max_dim, Blob<Dtype>* blob);
 
-	//template <typename Dtype>
-	//void netcdf_save_nd_dataset(
-	//    const hid_t file_id, const string& dataset_name, const Blob<Dtype>& blob,
-	//    bool write_diff = false);
-
 	int netcdf_load_int(int loc_id, const string& variable_name);
-	//void netcdf_save_int(hid_t loc_id, const string& variable_name, int i);
 	string netcdf_load_string(int loc_id, const string& variable_name);
-	//void netcdf_save_string(hid_t loc_id, const string& variable_name, const string& s);
 
-	//int netcdf_get_num_links(hid_t loc_id);
-	//string netcdf_get_name_by_idx(hid_t loc_id, int idx);
+
 
 }  // namespace caffe
 
